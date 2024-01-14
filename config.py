f87ba77e94f9418287f57df45135faab37a81051
‫youssef elzahar <49149335+youssefelzahar@users.noreply.github.com>
2024-01-14 09:18:47 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+config = {
+    "google_api_key": "AIzaSyCjGbCfbgLRatM2yngSvHtLwO3tgBS2e9k",
+    "youtube_playlist_id": "PLr3-bEi1N4khKZm2u6Q8pOLO-WNZrKDHn",
+    "kafka": {
+        "bootstrap.servers": "pkc-4r087.us-west2.gcp.confluent.cloud:9092",
+        "security.protocol": "sasl_ssl",
+        "sasl.mechanism": "PLAIN",
+        "sasl.username": "QK7JODAJIGYCG37F",
+        "sasl.password": "SchScpsF+Re6dBpRVZVygx5yWp9/ayCHAQUF2aXZOAevinxkPnjR1OdgjRBZtqmZ",
+    },
+    "schema_registry": {
+        "url": "https://psrc-9zg5y.europe-west3.gcp.confluent.cloud",
+        "basic.auth.user.info": "GELECPFXRK525KJU:lpTXcJhDUOHeY3MH3HKXEZOuFBQku5Il1/4fmqVyqu+ktNe3gA9s5tSPbzo08b29",
+    }
+}
